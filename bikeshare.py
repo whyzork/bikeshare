0f5d7d40fff74a8f1d101a37a8b2e02a94b30fad
Alfred Mancuso <alfred.c.mancuso@gmail.com>
2022-07-17 17:41:07 -0400

    Added output description to docstring of time_stats function.

--- bikeshare.py
+++ bikeshare.py
@@ -97,7 +97,12 @@ def load_data(city, month, day):
 # %%
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """
+    Displays statistics on the most frequent times of travel, inclding:
+        - Most common month
+        - Most common day of week
+        - Most common start hour
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
